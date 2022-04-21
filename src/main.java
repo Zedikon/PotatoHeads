f4ce20f33912672bb93c49b5ac735fca3e3d08e5
Zedikon <70371915+Zedikon@users.noreply.github.com>
2022-04-21 22:21:29 +0300

    Create main.java

---
+++ src/main.java
@@ -0,0 +1,15 @@
+package main;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class main extends JavaPlugin {
+	public void onEnable() {
+		Bukkit.getServer().getPluginCommand("givehead").setExecutor(new givehead());
+		Bukkit.getLogger().info("PotatoHeads enable!");
+	}
+	
+	public void onDisable() {
+		Bukkit.getLogger().info("PotatoHeads enable!");
+	}
+}
