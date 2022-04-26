a181fae90ad5359780308ba34e0c3f11427b7ec3
Zedikon <70371915+Zedikon@users.noreply.github.com>
2022-04-26 07:29:30 +0300

    1.1.0 Update

--- src/main.java
+++ src/main.java
@@ -4,9 +4,13 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class main extends JavaPlugin {
+	
 	public void onEnable() {
+		saveDefaultConfig();
+		Bukkit.getPluginManager().registerEvents(new Events(this), this);
 		Bukkit.getServer().getPluginCommand("givehead").setExecutor(new givehead());
 		Bukkit.getLogger().info("PotatoHeads enable!");
+	
 	}
 	
 	public void onDisable() {
