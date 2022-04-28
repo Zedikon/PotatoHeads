89552f7af2a667f8050fbb34695dbe8e47396208
Zedikon <70371915+Zedikon@users.noreply.github.com>
2022-04-28 20:13:56 +0300

    1.2.0 update

--- src/Events.java
+++ src/Events.java
@@ -15,17 +15,37 @@ public class Events implements Listener{
 	
 	@EventHandler
 	public void pd(PlayerDeathEvent e) {
-		// check enable this function or no
 		Boolean s = plugin.getConfig().getBoolean("EnableDropPlayerHeads");
+		int chance = plugin.getConfig().getInt("DropPlayerHeadChance");
+		// check enable this function or no
 		if(s == true) {
+			// check chance, if chance > 100, return warning
+			if(chance > 100) {
+				Bukkit.getLogger().warning("[PotatoHeads] Chance of drop player head > 100. Edit it in config.");
+			}
+				//check chance, if chance < 0, return warning
+			if(chance < 0) {
+				Bukkit.getLogger().warning("[PotatoHeads] Chance of drop player head < 0. Edit it in config.");
+			}else {
 			// trying give killer player head
-			try {
-				Player player = e.getEntity();
-				Player killer = player.getKiller();
-				String killername = killer.getName(); String playerhead = player.getName();
-				api.givehead(killername, playerhead);
-				Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "give " + killer.getName() + " player_head{SkullOwner: " + "\"" + player.getName() + "\"" + "}");
-			} catch (Exception ignore) { }
-		}
-	}	
+				try {
+					// chance for giving killer player head
+					int a = 0;
+					int b = 100;
+					int random_res = a + (int) (Math.random() * b);
+					
+					// give info about killer and player
+					Player player = e.getEntity();
+					Player killer = player.getKiller();
+					String killername = killer.getName(); String playerhead = player.getName();
+					
+					
+					if(random_res <= chance) {
+						api.givehead(killername, playerhead);
+					}
+					
+				} catch (Exception ignore) { }
+			}
+		}	
+	}
 }
